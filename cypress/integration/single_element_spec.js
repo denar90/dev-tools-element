a1dbb7ca176927938413190953597279d6e7d3ce
Artem Denysov <denysov.artem@gmail.com>
2017-11-18 01:46:15 +0200

    try 8000 port

--- cypress/integration/single_element_spec.js
+++ cypress/integration/single_element_spec.js
@@ -11,7 +11,7 @@ const appendDevTools = () => {
 
 describe('Single dev tools element', () => {
   before(() => {
-    cy.visit('http://localhost:8080');
+    cy.visit('http://localhost:8000');
   });
 
   it('should be shown with opened timeline tab', () => {
