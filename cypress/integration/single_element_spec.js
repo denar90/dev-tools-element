6715bb503cb4b3edcbc5cb0f9718d558282facc8
Artem Denysov <denysov.artem@gmail.com>
2017-11-19 01:22:11 +0200

    More tests (#7)
    * add tests for triggered events

--- cypress/integration/single_element_spec.js
+++ cypress/integration/single_element_spec.js
@@ -1,23 +1,13 @@
-const appendDevTools = () => {
-  cy.document().then(document => {
-    document.body.innerHTML = '';
-
-    const devToolsElement = document.createElement('dev-tools-element');
-    devToolsElement.setAttribute('src', 'https://www.dropbox.com/s/s1n8m8n5oddgoxx/timeline-docs.json?dl=0');
-    devToolsElement.setAttribute('class', 'dev-tools-test');
-    document.body.appendChild(devToolsElement);
-  });
-};
-
 describe('Single dev tools element', () => {
-  before(() => {
-    cy.visit('http://localhost:8000');
+  let devToolsElement;
+
+  beforeEach(() => {
+    cy.visit('http://localhost:8080');
+    devToolsElement = createDevToolsElement();
   });
 
   it('should be shown with opened timeline tab', () => {
-    window.location.reload(true);
-
-    appendDevTools();
+    attachDevToolsElement(devToolsElement);
 
     cy.get('iframe').then($frame => {
       // wait for dev tools scripts be loaded
@@ -28,4 +18,42 @@ describe('Single dev tools element', () => {
       });
     });
   });
+
+  context('Events', () => {
+    let readySpy, timelineLoadedSpy;
+
+    beforeEach(() => {
+      const eventHandlers = {
+        ready: () => {},
+        timelineLoaded: () => {}
+      };
+
+      readySpy = cy.spy(eventHandlers, 'ready');
+      timelineLoadedSpy = cy.spy(eventHandlers, 'timelineLoaded');
+
+      devToolsElement.addEventListener('DevToolsReady', eventHandlers.ready);
+      devToolsElement.addEventListener('DevToolsTimelineLoaded', eventHandlers.timelineLoaded);
+
+      attachDevToolsElement(devToolsElement);
+    });
+
+    it('should be triggered', () => {
+      cy.wait(4000).then(() => {
+        expect(readySpy).to.be.called;
+        expect(timelineLoadedSpy).to.be.called;
+      });
+    });
+  });
 });
+
+const createDevToolsElement = () => {
+  const devToolsElement = document.createElement('dev-tools-element');
+  devToolsElement.setAttribute('src', 'https://www.dropbox.com/s/s1n8m8n5oddgoxx/timeline-docs.json?dl=0');
+  return devToolsElement;
+};
+
+const attachDevToolsElement = devToolsElement => {
+  cy.document().then(document => {
+    document.body.appendChild(devToolsElement);
+  });
+};
